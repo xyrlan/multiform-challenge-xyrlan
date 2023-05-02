16c1928a3b69264408d86098663ed6c809704814
xyrlan; pedro <lobatopedro68@gmail.com>
2023-05-02 11:06:14 -0300

    updatae bugfix

--- src/components/thankyou.tsx
+++ src/components/thankyou.tsx
@@ -9,7 +9,7 @@ export default function Thankyou({data}) {
         <>
         <main className='flex flex-col items-center text-center text-[#02295a] md:pt-10 lg:px-10 xl:px-24 max-lg:px-10 md:px-4 max-md:justify-center max-md:pt-6 max-md:px-4 max-md:w-full w-full max-sm:pb-5'>
 
-            <Image src="./images/icon-thank-you.svg" width={80} height={80} alt="ThankYou" />
+            <Image src="/images/icon-thank-you.svg" width={80} height={80} alt="ThankYou" />
             <h1 className="mt-10 mb-5 text-4xl font-bold">Thank you, <span className="text-[#473dff]">{capitalizedFirstName}</span>!</h1>
             <p className="text-center text-gray-400"> Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com</p>
         </main>
